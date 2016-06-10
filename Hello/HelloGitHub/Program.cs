5a011e6596f2e2d4ef3089194726fefbb10eac67
Vikas Goyal <vikas3goyal@gmail.com>
2016-06-10 23:26:51 +0530

    v0.2

--- Hello/HelloGitHub/Program.cs
+++ Hello/HelloGitHub/Program.cs
@@ -10,7 +10,7 @@ namespace HelloGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow GitHub");
+            Console.WriteLine("Hello GitHub");
         }
     }
 }
